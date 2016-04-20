76a7269aa2c1132cb84600b3bcaadf05e20d9476
Jamie Sayer <sayertherebel@yahoo.com>
2016-04-20 11:01:03 +0100

    TimeSheets reminder initial commit

---
+++ WeeklyTimesheetsEmail/WeeklyTimesheetsEmail/TimesheetsEmail.ps1
@@ -0,0 +1,5 @@
+﻿#
+# Script.ps1
+#
+
+Send-MailMessage -SmtpServer 'silversmtp.silversands.co.uk' -To 'jamie.sayer@silversands.co.uk' -Subject 'Test' -Body 'Test!' -From 'simon.robinson@silversands.co.uk'
