6016910d9411160bbfd8fa017d2cf6b21f8d770f
Jamie Sayer <sayertherebel@yahoo.com>
2016-04-20 12:06:52 +0100

    WeeklyTimesheetReminder Initial Commit

---
+++ Runbooks/WeeklyTimesheetReminder.ps1
@@ -0,0 +1,4 @@
+
+# Weekly timesheet reminder 
+
+Send-MailMessage -SmtpServer silversmtp.silversands.co.uk -Body "Test!" -To "jamie.sayer@silversands.co.uk" -From "simon.robinson@silversands.co.uk" -Subject "Test"
