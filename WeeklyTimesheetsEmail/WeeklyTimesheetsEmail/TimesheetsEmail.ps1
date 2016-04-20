f4a394a266177c87617537b2ff0c92e8de7cf5a1
Jamie Sayer <sayertherebel@yahoo.com>
2016-04-20 12:06:25 +0100

    Dos

--- WeeklyTimesheetsEmail/WeeklyTimesheetsEmail/TimesheetsEmail.ps1
+++
@@ -1,5 +0,0 @@
-﻿#
-# Script.ps1
-#
-
-Send-MailMessage -SmtpServer 'silversmtp.silversands.co.uk' -To 'jamie.sayer@silversands.co.uk' -Subject 'Test' -Body 'Test!' -From 'simon.robinson@silversands.co.uk'
