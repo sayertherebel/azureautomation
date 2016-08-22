9fe51695007589392b38fdab17affd3f4833e59a
sayertherebel <sayertherebel@yahoo.com>
2016-08-22 10:34:13 +0100

    Created WfRecipientStatsToIOT.ps1 in AzureAutomation.

---
+++ Runbooks/WfRecipientStatsToIOT.ps1
@@ -0,0 +1,19 @@
+workflow WfRecipientStatsToIOT
+{
+	Param($messages)
+	Write-Output ("{0} Messages in scope..." -f $messages.Count)
+	
+	$checkpointCounter = 0
+	
+	foreach ($message in $messages)
+	{
+		$checkpointCounter++
+		if($checkpointCounter -eq 20)
+		{
+			$checkpointCounter = 0
+			CheckPoint-Workflow
+		}
+		
+        Push-IoTEvent -strConn "HostName=IotHubRecipientStats.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=SCwNpfWZaAgJTT1IMPRuvMpXon6cGKWFFZlqyM+thOc=" -strIOTDevice "TestTwo" -strHubURI IotHubRecipientStats.azure-devices.net -objDatagram @{"ReceivedUtcTime"=$message.Received.ToString(); "SenderAddress"=$message.SenderAddress;"RecipientAddress"=$message.RecipientAddress;"Direction"=$message.direction} -aSyncSend
+	}
+}
